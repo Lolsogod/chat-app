9f2a5101a00fa448bf58710a68c78c6e502ba4ca
lolso good <MY_NAME@example.com>
2023-05-24 15:39:08 +0400

    almost best commit

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,9 +2,7 @@
 import Messages from "./Messages"
 import Input from "./Input"
 
-
-export default function Chat() {
-  
+export default function Chat() {  
 
   return (
     <div className="flex flex-col chat-c bg-slate-950 p-2 justify-end items-center">
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,11 +3,11 @@ import { useEffect, useState } from "react";
 import { chatApi } from "./api/ChatApi";
 import { useKeycloak } from "@react-keycloak/web";
 import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
+import Stomp, { Client } from 'stompjs';
 
 export default function SideBar({setSelected, selected}: any) {
   
-
+  const [client, setClient] = useState<Stomp.Client | null>()
   const { keycloak } = useKeycloak()
   
   const URL = 'http://127.0.0.1:9080/chat';
@@ -22,31 +22,47 @@ export default function SideBar({setSelected, selected}: any) {
       })
   }
   
-  function connectWS() {
+  const connectWS = async ()=> {
     const ws = new SockJS(`${URL}`);
   
-    let client: Stomp.Client | null = Stomp.over(ws);
-  
-    client.connect({}, () => {
-      client!.subscribe("/topic/messages/" + user, (mes: any) => {
-        console.log(mes);
-      });
-    });
+    await setClient(Stomp.over(ws))
+    console.log(client)
   }
 
   const refresh = async ()=>{
     await chatApi.fetchUsers(keycloak.token)
       .then((res)=>setUserList(res.data))
   }
+  //test
 
+  function sendMsg(from: any, text: any) {
+    client!.send("/app/chat/" + selected, {}, JSON.stringify({
+        fromLogin: from,
+        message: text
+    }));
+  }
+    //test-end
   useEffect(()=>{
     refresh()
   },[])
+  useEffect(()=>{
+    console.log("----------------")
+    console.log(client)
+    if (client){
+      client!.connect({}, () => {
+      client!.subscribe("/topic/messages/" + user, (mes: any) => {
+        console.log(mes);
+      });
+    });
+    }
+    
+  },[client])
   return (
     <div className="flex flex-1 flex-col bg-slate-900 p-2 gap-2 overflow-y-scroll">
       <input type="text" value={user} onChange={(e) => setUser(e.target.value)}></input>
         <button onClick={addUser}>add user</button>
         <button onClick={refresh}>refresh</button>
+        <button onClick={() => sendMsg(user, "hi")}>say hi</button>
         {userList.map((usr, i) => (
           <ChatItem key={i} name={usr} setSelected={setSelected} selected={selected}/>
         ))}
